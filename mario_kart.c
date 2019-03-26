994b40ddabd67a62cffe501f7bcec442b7bf2065
Nikola Kosaric <nikolakosaric@gmail.com>
2019-03-26 19:50:54 -0400

    Prettied up some details, still need to add documentation

--- mario_kart.c
+++ mario_kart.c
@@ -33,11 +33,11 @@ struct mario_kart
     uint32_t ts;   // time without a collision to determine speed
     uint32_t st;   // start time of the race
     uint32_t ft;   // finishing time of the race
+    uint8_t ss;
     uint32_t t; // time of race
     uint16_t i; //
     uint8_t id; ///< ID of game
 };
-
 
 static struct mario_kart game;
 
@@ -54,32 +54,37 @@ static void MoveRight(void);
 static void DetectCollision(void);
 static void SetSpeed(void);
 
-uint8_t leftLimits[WINDOW_HEIGHT-1] = {7, 0, 0, 0, 0, 0, 0, 0, 0};
+static void FinishRace(void);
 
+uint8_t leftLimits[WINDOW_HEIGHT - 1] = { 7, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 void Mario_Kart_Init()
 {
     // Register the module with the game system and give it the name "MUH3"
     game.id = Game_Register("Mario", "Race", Play, Help);
+    Game_ClearScreen();
 }
 
 void Help(void)
 {
     Game_Printf(
             "Press 'a' to turn the car left\n and 'd' to turn the car right.");
+    Game_ClearScreen();
 }
 
 void Play(void)
 {
-    // clear the screen
+    // clear the screenca
     Game_ClearScreen();
     Game_HideCursor();
 
     // draw a box around our map
+    Game_SetColor(BackgroundBlack);
     Game_DrawRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     game.x = 12;
     game.c = '^';
+    game.ss = 5;
     game.s = 175;
     game.i = 0;
     game.ts = TimeNow();
@@ -89,84 +94,101 @@ void Play(void)
     Task_Schedule(drawTrack, 0, 1000, game.s);
 }
 
-void newTrackWidth() {
+void newTrackWidth()
+{
     uint8_t y = 8;
-    for (y = WINDOW_HEIGHT - 2; y > 0; y--) {
-        leftLimits[y] = leftLimits[y-1];
+    for (y = WINDOW_HEIGHT - 2; y > 0; y--)
+    {
+        leftLimits[y] = leftLimits[y - 1];
     }
     uint8_t min = leftLimits[1] - 2;
     uint8_t max = leftLimits[1] + 2;
-    if (min <= 2) {min = 2;}
-    if (max >= MAXSPACE) {max = MAXSPACE;}
+    if (min <= 2)
+    {
+        min = 2;
+    }
+    if (max >= MAXSPACE)
+    {
+        max = MAXSPACE;
+    }
     leftLimits[0] = random_int(min, max);
 
 }
 
-void drawTrack() {
+void drawTrack()
+{
     uint8_t x = 1;
     uint8_t y = 1;
 
-    for (y = 1; y < WINDOW_HEIGHT; y++){
-        for (x = 1; x < WINDOW_WIDTH; x++){
-            if (x <= leftLimits[y-1]){
+    for (y = 1; y < WINDOW_HEIGHT; y++)
+    {
+        for (x = 1; x < WINDOW_WIDTH; x++)
+        {
+            if (x <= leftLimits[y - 1])
+            {
                 Game_CharXY('-', x, y);
             }
-            else if (leftLimits[y-1] <= 0){
-                    Game_CharXY(' ', x, y);
+            else if (leftLimits[y - 1] <= 0)
+            {
+                Game_CharXY(' ', x, y);
             }
-            else if (x <= leftLimits[y-1] + TRACK_WIDTH){
+            else if (x <= leftLimits[y - 1] + TRACK_WIDTH)
+            {
                 Game_CharXY(' ', x, y);
 
-            } else {
+            }
+            else
+            {
                 Game_CharXY('-', x, y);
             }
         }
-
     }
 
-    game.i ++;
+    game.i++;
 
-    if (game.i == MAXCYCLES) {
-        Game_SetColor(BackgroundBlack);
-        Task_Remove(drawTrack, 0);
-        game.ft = TimeNow();
-        game.t = (game.ft - game.st) / 1000;
-        Game_ClearScreen();
-        Game_Printf("YOU WIN!\r\n");
-        Game_Printf("Your time was %d seconds!", game.t);
+    if (game.i == MAXCYCLES)
+    {
+        FinishRace();
     }
-    else {
+    else
+    {
         Game_CharXY(game.c, game.x, WINDOW_HEIGHT - 1);
+        Game_CharXY(game.ss + '0', WINDOW_WIDTH + 1, 1);
         DetectCollision();
         newTrackWidth();
         SetSpeed();
     }
 }
 
-void Receiver(uint8_t c) {
-    if (game.i != MAXCYCLES) {
-        switch (c) {
-            case 'a':
-            case ',':
-            case 'A':
-            case '<':
-                MoveLeft();
-                break;
-            case 'd':
-            case 'D':
-            case '.':
-            case '>':
-                MoveRight();
-                break;
-            default:
-                break;
+void Receiver(uint8_t c)
+{
+    if (game.i != MAXCYCLES)
+    {
+        switch (c)
+        {
+        case 'a':
+        case ',':
+        case 'A':
+        case '<':
+            MoveLeft();
+            break;
+        case 'd':
+        case 'D':
+        case '.':
+        case '>':
+            MoveRight();
+            break;
+        default:
+            break;
         }
     }
 }
 
-void MoveRight() {
+void MoveRight()
+{
     // make sure we can move right
-    if (game.x < WINDOW_WIDTH - 1) {
+    if (game.x < WINDOW_WIDTH - 1)
+    {
         // clear location
         Game_CharXY(' ', game.x, WINDOW_HEIGHT - 1);
         game.x++;
@@ -175,9 +197,11 @@ void MoveRight() {
     }
 }
 
-void MoveLeft() {
+void MoveLeft()
+{
     // make sure we can move right
-    if (game.x > 1) {
+    if (game.x > 1)
+    {
         // clear location
         Game_CharXY(' ', game.x, WINDOW_HEIGHT - 1);
         game.x--;
@@ -186,33 +210,59 @@ void MoveLeft() {
     }
 }
 
-void DetectCollision() {
-    if ((game.x <= leftLimits[WINDOW_HEIGHT - 2]) || (game.x >= (leftLimits[WINDOW_HEIGHT - 2] + TRACK_WIDTH))) {
-        game.ts = 0;
-        Game_SetColor(BackgroundRed);
-    } else {
-        game.ts ++;
-        Game_SetColor(BackgroundGreen);
+void DetectCollision()
+{
+    if ((game.x <= leftLimits[WINDOW_HEIGHT - 2])
+            || (game.x >= (leftLimits[WINDOW_HEIGHT - 2] + TRACK_WIDTH + 1)))
+    {
+        if (leftLimits[WINDOW_HEIGHT - 2] != 0)
+        {
+            game.ts = 0;
+            Game_SetColor(BackgroundRed);
+        }
+    }
+    else
+    {
+        game.ts++;
+        Game_SetColor(BackgroundBlack);
     }
 }
 
-void SetSpeed () {
-    switch(game.ts) {
-        case 0:
-            game.s = 1000;
-            break;
-        case 10:
-            game.s = 750;
-            break;
-        case 30:
-            game.s = 500;
-            break;
-        case 50:
-            game.s = 250;
-            break;
-        case 75:
-            game.s = 175;
-            break;
+void SetSpeed()
+{
+    switch (game.ts)
+    {
+    case 0:
+        game.ss = 1;
+        game.s = 1000;
+        break;
+    case 10:
+        game.ss = 2;
+        game.s = 750;
+        break;
+    case 30:
+        game.ss = 3;
+        game.s = 500;
+        break;
+    case 50:
+        game.ss = 4;
+        game.s = 250;
+        break;
+    case 75:
+        game.ss = 5;
+        game.s = 175;
+        break;
     }
     Task_ChangePeriod(drawTrack, game.s, 1);
+}
+
+void FinishRace()
+{
+    Game_SetColor(BackgroundBlack);
+    Task_Remove(drawTrack, 0);
+    game.ft = TimeNow();
+    game.t = (game.ft - game.st) / 1000;
+    Game_ClearScreen();
+    Game_Printf("YOU WIN!\r\n");
+    Game_Printf("Your time was %d seconds!", game.t);
 }
